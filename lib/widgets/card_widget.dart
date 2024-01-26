dc76571aaa03388cbf260c8d9c28f6243aee97a3
firdaus <nanafirdaus02@gmail.com>
2024-01-26 07:54:32 +0100

    made some changes to the card widget

--- lib/widgets/card_widget.dart
+++ lib/widgets/card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:fintech_app_ui/utils/extension.dart';
 import 'package:flutter/material.dart';
 
 class CardWidget extends StatelessWidget {
@@ -11,13 +12,13 @@ class CardWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
-          height: 150,
-          width: 275,
+          height: context.screenHeight * .22,
+          width: context.screenWidth * .8,
           decoration: const BoxDecoration(
             borderRadius: BorderRadius.all(
               Radius.circular(5),
             ),
-            color: Colors.blue,
+            color: Color.fromARGB(255, 28, 58, 107),
           ),
           child: const Padding(
             padding: EdgeInsets.all(10.0),
@@ -28,8 +29,14 @@ class CardWidget extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Mint Wallet"),
-                    Icon(Icons.wallet_rounded),
+                    Text(
+                      "Mint Wallet",
+                      style: TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                    Icon(
+                      Icons.wallet_rounded,
+                      color: Colors.white,
+                    ),
                   ],
                 ),
                 Text(
@@ -37,20 +44,40 @@ class CardWidget extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text("Mfonabasi Udoka"),
-                    Icon(Icons.more_horiz),
+                    Text(
+                      "Mfonabasi Udoka",
+                      style: TextStyle(color: Colors.white, fontSize: 13),
+                    ),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.white,
+                    ),
                   ],
                 )
               ],
             ),
           ),
         ),
-        const Icon(Icons.add_circle),
+        Container(
+          margin: const EdgeInsets.only(right: 3),
+          width: context.screenWidth * 0.05,
+          height: context.screenHeight * 0.05,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 28, 58, 107),
+            shape: BoxShape.circle,
+          ),
+          child: const Icon(
+            Icons.add,
+            size: 12,
+            color: Colors.white,
+          ),
+        ),
       ],
     );
   }
